ecd2029b57c30bac61f0f39c7ec57d53003e1202
Flavio Marques <flavio.marques01@etec.sp.gov.br>
2025-05-24 10:36:02 -0300

    feat: working face detections

--- components/face-recognition-container.tsx
+++ components/face-recognition-container.tsx
@@ -1,11 +1,27 @@
 "use client";
 
-import { useRef } from "react";
+import { useEffect, useRef } from "react";
 
+import { handleFaceDetections } from "@/lib/face-api/handle-face-detections";
 import { WebcamContainer } from "./composites/webcam-container";
+import { loadModels } from "@/lib/face-api/load-models";
 
 export const FaceRecognitionContainer = () => {
+  const overlayRef = useRef<HTMLCanvasElement>(null);
   const videoRef = useRef<HTMLVideoElement>(null);
 
-  return <WebcamContainer videoRef={videoRef} />;
+  useEffect(() => {
+    loadModels();
+  }, []);
+
+  useEffect(() => {
+    const intervalId = setInterval(
+      () => handleFaceDetections({ videoRef, overlayRef }),
+      500,
+    );
+
+    return () => clearInterval(intervalId);
+  }, [videoRef, overlayRef]);
+
+  return <WebcamContainer overlayRef={overlayRef} videoRef={videoRef} />;
 };

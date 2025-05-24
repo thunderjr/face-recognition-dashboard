bc9acf124fb4045ae148aa779b9a600efdcaf8cd
Flavio Marques <flavio.marques01@etec.sp.gov.br>
2025-05-24 10:35:34 -0300

    chore: fix custom detection type

--- lib/face-api/types.ts
+++ lib/face-api/types.ts
@@ -1,7 +1,10 @@
 import { Expression, Gender } from "@/types";
+import * as faceapi from "face-api.js";
 
+// Same as faceapi.WithAge<faceapi.WithGender<faceapi.WithFaceExpressions<{ detection: faceapi.FaceDetection; }>>>
 export type RawDetectionResult = {
   expressions: Record<Expression, number>;
+  detection: faceapi.FaceDetection;
   genderProbability: number;
   gender: Gender;
   age: number;

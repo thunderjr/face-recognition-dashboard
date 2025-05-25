8787985fa373fc84e01290b70bf762b9916bd432
Flavio Marques <flavio.marques01@etec.sp.gov.br>
2025-05-25 19:40:04 -0300

    fix: add app config context provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,29 +1,37 @@
-import type React from "react"
-import type { Metadata } from "next"
-import { Inter } from "next/font/google"
-import "./globals.css"
-import { ThemeProvider } from "@/components/theme-provider"
+import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] })
+import { Inter } from "next/font/google";
+import type { Metadata } from "next";
+import type React from "react";
+
+import { ThemeProvider } from "@/components/theme-provider";
+import { AppConfigProvider } from "@/context/app-config-context";
+
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Dashboard de Reconhecimento Facial",
   description: "Monitoramento de fluxo de pessoas por reconhecimento facial",
-    generator: 'v0.dev'
-}
+  generator: "v0.dev",
+};
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
   return (
     <html lang="pt-BR" suppressHydrationWarning>
       <body className={inter.className}>
-        <ThemeProvider attribute="class" defaultTheme="dark" enableSystem disableTransitionOnChange>
-          {children}
+        <ThemeProvider
+          enableSystem
+          attribute="class"
+          defaultTheme="dark"
+          disableTransitionOnChange
+        >
+          <AppConfigProvider>{children}</AppConfigProvider>
         </ThemeProvider>
       </body>
     </html>
-  )
+  );
 }
